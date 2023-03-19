fe93c0e090d04cd7e3670f9cddb3d4d5b01fdf6f
Peter Aryanto <peter.aryanto@gmail.com>
2023-03-20 02:23:37 +1100

    Example execute JS MORE (unclear): https://www.selenium.dev/selenium/docs/api/javascript/module/selenium-webdriver/index_exports_ThenableWebDriver.html

--- tests/load-feeds.js
+++ tests/load-feeds.js
@@ -6,8 +6,19 @@ async function testOpen() {
     .build();
   await driver.get('https://transferprice-energysolution-bluenrg.dynamic.int.cd8.cloud/LoadFeeds');
 
+  /*
   const buttons = await driver.findElements(By.className('icm-plus2'));
   await buttons[0].click();
+  */
+
+  await driver.executeAsyncScript(
+      "const expectedId = '45313';"
+      + "const idLinks = Array.from(document.getElementsByTagName('a'));"
+      + "const topIdLink = idLinks.filter((l) => l.innerText == expectedId);"
+      + "topIdLink[0].click();"
+      // BACKUP plan right below also works!
+      //+ "if (topIdLink[0].innerText == expectedId) navigateEdit('LoadFeed', expectedId);"
+    );
 }
 
 testOpen();
