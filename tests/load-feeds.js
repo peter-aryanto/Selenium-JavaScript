c447f4d15b9e4c92b0c8cd85c56bf9d169f8f87a
Peter Aryanto <peter.aryanto@gmail.com>
2023-03-20 01:46:31 +1100

    Example find element then click

--- tests/load-feeds.js
+++ tests/load-feeds.js
@@ -1,11 +1,13 @@
-const WebDriver = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 
 async function testOpen() {
-  const driver = await new WebDriver.Builder()
+  const driver = await new Builder()
     .forBrowser('MicrosoftEdge')
     .build();
-  //await driver.get('https://transferprice-energysolution-bluenrg.dynamic.int.cd8.cloud/LoadFeeds');
-  await driver.get('https://google.com');
+  await driver.get('https://transferprice-energysolution-bluenrg.dynamic.int.cd8.cloud/LoadFeeds');
+
+  const buttons = await driver.findElements(By.className('icm-plus2'));
+  await buttons[0].click();
 }
 
 testOpen();
